faedca2568f5c3e5230290a7bbec8d56039b0c04
Luiz Arlochi <luiz.arlochi@gmail.com>
2024-11-04 17:14:27 -0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/Aeronaval.github.io",
   build: {
     chunkSizeWarningLimit: 3000,
   }
